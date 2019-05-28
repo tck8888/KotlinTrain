690a7197b77d976e1e12049c19b7f4afe0e0b515
tck8888 <380054728@qq.com>
2019-05-28 23:11:46 +0800

    kotlin基本数据类型

---
+++ HelloKotlin/src/BasicTypes.kt
@@ -0,0 +1,22 @@
+/**
+ *
+ *   @Author:tck
+ *   @Description:基本数据类型
+ *   @Date:2019/5/28
+ *
+ **/
+val aBoolean:Boolean =true
+
+val anInt:Int = 8
+
+val aLong:Long = 123
+
+val aFloat:Float = 2.0f
+
+val aDouble:Double = 3.0
+
+val aShort:Short = 4
+
+val aByte:Byte = Byte.MAX_VALUE
+
+
