cf2618d22b51251c749fd768b92aaab016829921
tck <tck@dra100.com>
2019-05-29 13:06:23 +0800

    常量与变量

---
+++ HelloKotlin/src/chapter2/ValAndVar.kt
@@ -0,0 +1,16 @@
+package chapter2
+
+/**
+ * description:</br>
+ * created on: 2019/5/29 12:59</br>
+ * @author tck
+ * @version 1.0
+ *
+ */
+//不可以改变值
+//运行常量
+val FINAL_HELLO = "hell"
+//编译期常量
+const val CONST_FINAL_HELLO = "hell"
+//变量
+var hello = "hell"
