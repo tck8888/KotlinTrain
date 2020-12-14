f49390b719fcaa154130b558eba042b4ef083f52
tck <380054728@qq.com>
2020-12-14 20:37:36 +0800

    for循环

---
+++ src/main/java/com/tck/kotlintrain/forDemo/ForTest.kt
@@ -0,0 +1,12 @@
+package com.tck.kotlintrain.forDemo
+
+fun main() {
+
+    for (i in 0 until 3){
+        println(i)
+    }
+    println("=======================")
+    for (i in 0..3){
+        println(i)
+    }
+}
